dcde51405e52a9e4a9be88eb24b81644f48bdea4
Robert Bethencourt <robert.bethencourt@gmail.com>
2016-06-25 11:12:29 -0400

    data added

---
+++ app/data/table-data.js
@@ -0,0 +1,16 @@
+var table_data = [
+
+	{
+		name: Bill,
+		Phone: 5555555,
+		email: bill@bill.com,
+		uniqueId: 1	
+	},
+	{
+		name: Wendy,
+		Phone: 7777777,
+		email: wendy@wendy.com,
+		uniqueId: 2
+	}
+	
+]
